6d7f2c002604539e540bbc8fa792176bdbf04869
boopesh-foxy <boopesh.mc@gmail.com>
2025-11-05 14:38:56 +0530

    iam role creation - update

--- aws_iam_role.py
+++ aws_iam_role.py
@@ -1,18 +1,14 @@
-import boto3
 import json
-from botocore.exceptions import ClientError
-
-iam = boto3.client("iam")
-
+from aws_clients import iam_client
+client = iam_client()
 
 def ensure_iot_to_sqs_role(queue_arn, role_name = "iot_to_sqs_role"):
     """ ensures an iam role exists for AWS IoT to send messages to SQS. Returns the role arn."""
     try:
-        response_existense = iam.get_role(role_name)
+        response_existense = client.get_role(role_name)
         print(f"Reusing existing role: {role_name}")
         return response_existense["Role"]["Arn"]
-    except ClientError as e:
-        print(":: Error ::",e)
+    except Exception as e:
         print(f"Role {role_name} not found. Creating...")
 
     trust_policy = {
@@ -26,14 +22,12 @@ def ensure_iot_to_sqs_role(queue_arn, role_name = "iot_to_sqs_role"):
         ],
     }
 
-    response_role_creation = iam.create_role(
+    response_role_creation = client.create_role(
         RoleName=role_name,
         AssumeRolePolicyDocument=json.dumps(trust_policy),
         Description="Role for AWS IoT Core to send messages to SQS",
     )
-
     role_arn = response_role_creation["Role"]["Arn"]
-
     
     policy_name = f"{role_name}-sqs-access"
     policy_doc = {
@@ -51,11 +45,10 @@ def ensure_iot_to_sqs_role(queue_arn, role_name = "iot_to_sqs_role"):
         ],
     }
 
-    iam.put_role_policy(
+    client.put_role_policy(
         RoleName=role_name,
         PolicyName=policy_name,
         PolicyDocument=json.dumps(policy_doc),
     )
 
-    print("+ Attached inline policy for SQS access")
     return role_arn
