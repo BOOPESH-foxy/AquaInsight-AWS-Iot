5917b8930e9ac9967ed9273e2b3888fe6e29afe6
boopesh-foxy <boopesh.mc@gmail.com>
2025-11-08 16:35:56 +0530

    mqtt client listener

--- sensor_data_operations/mqtt_listener_client.py
+++ sensor_data_operations/mqtt_listener_client.py
@@ -1,10 +1,28 @@
-from awsiot import mqtt_connection_builder
+import os
+import json
+from dotenv import load_dotenv
 from awscrt import io,mqtt
+from awsiot import mqtt_connection_builder
 from sensor_data_operations.tank_metadata import topic,endpoint
 
+load_dotenv()
 
-def response_message_processor():
-    pass
+endpoint = os.getenv("ENDPOINT")
+
+def response_message_processor(payload):
+    try:
+        data = json.loads(payload.decode("utf-8"))
+    except json.JSONDecodeError:
+        print("  Raw payload:", payload)
+        return 
+    
+    print("  Payload:", json.dumps(data, indent=2))
+    cl_actions = data.get("command")
+    if(cl_actions == "Dispense Cl"):
+        pass
+    elif(cl_actions == "No-changes"):
+        pass
+
 
 def main():
     mqtt_connection = mqtt_connection_builder.mtls_from_path(
